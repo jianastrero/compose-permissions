480361b88157230a0f3c39ee8c96029e9d854b14
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-20 03:21:02 +0800

    Update androidx.core:core-ktx version and add "compose-permissions" module
    This commit updates the version of androidx.core:core-ktx from 1.9.0 to 1.10.1 for improved stability and features. In addition, a new Android library module, "compose-permissions", has been added to handle composed permissions and accordingly, build.gradle.kts has been modified.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -56,7 +56,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation(project(":compose-permissions"))
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
