9fdca9135f111ab7b23a6772aa40b99839aa1457
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-20 03:51:21 +0800

    Update MainActivity functionality for permission request
    MainActivity has been extensively updated to handle camera permissions. Instead of the previous "Hello World" placeholder, it now includes a "request permissions" button. This button triggers a request for camera permission if not already granted. A text field has also been added to display whether the permission is granted, enhancing user feedback.

--- example/src/main/java/dev/jianastrero/compose_permissions/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_permissions/example/MainActivity.kt
@@ -3,30 +3,59 @@ package dev.jianastrero.compose_permissions.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.jianastrero.compose_permissions.composePermission
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                MainContent()
-            }
+            MainContent()
         }
     }
 }
 
 @Composable
 private fun MainContent() {
-    MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            Text(text = "Hello World")
+    val cameraPermission = composePermission(permission = android.Manifest.permission.CAMERA)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Granted: ${cameraPermission.isGranted}",
+            color = if (cameraPermission.isGranted) Color.Green else Color.Red,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Black
+        )
+        Button(
+            onClick = {
+                if (cameraPermission.isGranted) {
+                    /* Do Something */
+                } else {
+                    cameraPermission.request()
+                }
+            },
+            modifier = Modifier.padding(top = 12.dp)
+        ) {
+            Text(text = "Request Permission")
         }
     }
 }
