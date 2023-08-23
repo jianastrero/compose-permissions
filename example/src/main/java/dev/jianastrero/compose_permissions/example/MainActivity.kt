8a528ea93363962924799b21566f92103ea73b1a
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-23 21:27:08 +0800

    Update MainActivity.kt, add a snackbar for camera permission rationale
    Enhanced camera permission request in ComposePermission class in MainActivity.kt file by adding a snackbar which displays a rationale message. If the camera permission rationale should be shown, a snackbar message is displayed to inform the user to grant permission. If the rationale does not need to be shown, the camera permission is requested directly. This gives a better understanding for the users as to why the permissions are needed and enhances UX transparency.

--- example/src/main/java/dev/jianastrero/compose_permissions/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_permissions/example/MainActivity.kt
@@ -3,34 +3,122 @@ package dev.jianastrero.compose_permissions.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.jianastrero.compose_permissions.composePermission
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
+            MainScreen()
         }
     }
 }
 
 @Composable
-private fun MainContent() {
+private fun MainScreen() {
+    val snackBarHostState = remember { SnackbarHostState() }
     val cameraPermission = composePermission(permission = android.Manifest.permission.CAMERA)
+    val coroutineScope = rememberCoroutineScope()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        MainContent(
+            isGranted = cameraPermission.isGranted,
+            requestPermission = {
+                if (cameraPermission.shouldShowRationale) {
+                    coroutineScope.launch {
+                        snackBarHostState.showSnackbar(
+                            "Please allow the permission to use the camera.",
+                            "Ok",
+                        )
+                    }
+                } else {
+                    cameraPermission.request()
+                }
+            }
+        )
+        SnackbarHost(
+            hostState = snackBarHostState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+        ) { snackbarData ->
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(Color.Black)
+            ) {
+                Text(
+                    text = snackbarData.visuals.message,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 12.dp, top = 12.dp, end = 12.dp)
+                )
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextButton(
+                        onClick = {
+                            snackbarData.dismiss()
+                        }
+                    ) {
+                        Text(
+                            text = "Dismiss",
+                            color = Color.Red
+                        )
+                    }
+                    TextButton(
+                        onClick = {
+                            cameraPermission.request()
+                            snackbarData.dismiss()
+                        }
+                    ) {
+                        Text(
+                            text = snackbarData.visuals.actionLabel ?: "Ok",
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainContent(
+    isGranted: Boolean,
+    requestPermission: () -> Unit
+) {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -40,17 +128,17 @@ private fun MainContent() {
             .padding(24.dp)
     ) {
         Text(
-            text = "Granted: ${cameraPermission.isGranted}",
-            color = if (cameraPermission.isGranted) Color.Green else Color.Red,
+            text = "Granted: $isGranted",
+            color = if (isGranted) Color.Green else Color.Red,
             fontSize = 24.sp,
             fontWeight = FontWeight.Black
         )
         Button(
             onClick = {
-                if (cameraPermission.isGranted) {
+                if (isGranted) {
                     /* Do Something */
                 } else {
-                    cameraPermission.request()
+                    requestPermission()
                 }
             },
             modifier = Modifier.padding(top = 12.dp)
@@ -63,5 +151,5 @@ private fun MainContent() {
 @Preview
 @Composable
 private fun MainContentPreview() {
-    MainContent()
+    MainContent(isGranted = true, requestPermission = { /* Do Nothing */ })
 }
